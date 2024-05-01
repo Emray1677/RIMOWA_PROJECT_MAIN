733162c15f0a7d61e239e2c97d520ccd1bbe536d
Emray <mosesemray@gmail.com>
2024-05-01 20:23:49 +0100

    jfj

--- merchantDashboard.js
+++ merchantDashboard.js
@@ -1,30 +1,64 @@
-let MerchantCategoriesBtn = document.getElementById('MerchantCategoriesBtn');
+// let MerchantCategoriesBtn = document.getElementById('MerchantCategoriesBtn');
 
-let createProductDiv = document.getElementById('createProductDiv')
-let CreateCatNav = document.getElementById('CreateCatNav')
-let CreateProNav = document.getElementById('CreateProNav')
-let ProCreatedNav = document.getElementById('ProCreatedNav')
-let CatCreatedNav = document.getElementById('CatCreatedNav')
-let OrdersNav = document.getElementById('OrdersNav')
+// let createProductDiv = document.getElementById('createProductDiv')
+// let CreateCatNav = document.getElementById('CreateCatNav')
+// let CreateProNav = document.getElementById('CreateProNav')
+// let ProCreatedNav = document.getElementById('ProCreatedNav')
+// let CatCreatedNav = document.getElementById('CatCreatedNav')
+// let OrdersNav = document.getElementById('OrdersNav')
 
-let CategoriesAddition = document.getElementById('CategoriesAddition')
-let createProduct = document.getElementById('createProduct')
+// let CategoriesAddition = document.getElementById('CategoriesAddition')
+// let createProduct = document.getElementById('createProduct')
 
-function hidedisplay(){
-    CategoriesAddition.style.display = 'none';
-    createProduct.style.display = 'block';
+// function hidedisplay(){
+//     CategoriesAddition.style.display = 'none';
+//     createProduct.style.display = 'block';
    
     
-}
-
-CreateProNav.addEventListener('click', hidedisplay)
-
-
-// function createProNavHideDisplay(){
-//     createProduct.style.display = 'none';
-//     CategoriesAddition.style.display = 'block';
-   
 // }
 
-// CreateProNav.addEventListener('click', createProNavHideDisplay)
+// CreateProNav.addEventListener('click', hidedisplay)
+
+let creatingcategorydropbtn = document.getElementById('creatingcategorydropbtn');
+let Categoriesdiv = document.getElementById('Categoriesdiv');
+
+function firstbtndropdowncat(){
+    Categoriesdiv.style.display = 'block';
+}
+
+function secondbtndropdowncat(){
+    Categoriesdiv.style.display = 'none';
+}
+
+let CategoryName = document.getElementById('CategoryName');
+let Categoryimage = document.getElementById('Categoryimage');
+
+
+function CreatingCategory(){
+
+  let CreateCategoryInputDetails = {
+    merchant_id: "662ccac2f7192e0c5d7043e4",
+    name: CategoryName.value,
+    image: Categoryimage.value,
+  }
+
+  console.log(CreateCategoryInputDetails);
+ 
+  $(document).ready(function () {
+    $.ajax({
+        type: "post",
+        url: "http://ecommerce.reworkstaging.name.ng/v2/categories",
+        header: "application/json",
+        data: CreateCategoryInputDetails,
+        success: function (res) {
+            console.log(res);
+        },
+        error: function (error) {
+            console.log("error", error);
+        }
+    })
+})
+}
+
+
 
